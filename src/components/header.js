023ee14e6904d9bb731bcc23ec55ae193fdab227
tenzyu <tenzyu.on@gmail.com>
2021-01-09 23:47:00 +0900

    refactoring

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default () => (
-  <header class="flex sticky z-50 top-0 left-0 h-16 border-b opacity-90 bg-white mb-4 justify-around">
-    <h1 class="my-auto ml-5 text-xl font-logo">
+  <header class="font-logo flex justify-around sticky top-0 left-0 z-50 h-16 border-b opacity-90 bg-white mb-5">
+    <h1 class="my-auto ml-5 text-xl">
       <Link to={"/"}>HOME</Link>
     </h1>
-    <nav class="my-auto mr-5 font-logo w-1/3">
+    <nav class="my-auto mr-5 w-1/3">
       <ul class="flex justify-around">
       {/* {navs.map(nav => (
         <li><Link to={nav.link}>{nav.name}</Link></li>
