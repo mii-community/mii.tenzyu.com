85708e0cdb129777ea5de29b1f06f8d0f0043154
tenzyu <tenzyu.on@gmail.com>
2021-01-09 23:31:04 +0900

    prepare 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,15 @@
 import * as React from "react"
+import Layout from "../layouts/default"
+import Meta from "../components/meta"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
+export default () => (
+    <Layout>
+        <Meta title="404" />
+        <main class="text-center">
+          <h2 class="my-16 text-4xl">Page Not Found</h2>
+          <h3 class="text-blue-500 text-xl"><Link to="/">Go Back</Link></h3>
+        </main>
+    </Layout>
+);
