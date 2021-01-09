bcf82e66460d2b047342811f863c32d65b3fc40e
tenzyu <tenzyu.on@gmail.com>
2021-01-09 23:29:30 +0900

    put footer

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <footer class="mt-4 flex justify-end border-t mb-5">
+    <p class="font-thin text-xs mr-5 pt-1">© Tenzyu Masuda.</p>
+  </footer>
+)
