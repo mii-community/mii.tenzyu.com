6cf67053dcff8483914b850ebeaa31f9a0987b55
tenzyu <tenzyu.on@gmail.com>
2021-01-09 23:19:33 +0900

    use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'logo': ['arial black', 'Helvetica Neue', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
