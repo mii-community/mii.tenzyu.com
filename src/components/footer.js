e0e4d9245a74491b1345cef6065b473b8ba074f4
tenzyu <tenzyu.on@gmail.com>
2021-01-09 23:45:57 +0900

    fixed footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <footer class="mt-4 flex justify-end border-t mb-5">
-    <p class="font-thin text-xs mr-5 pt-1">© Tenzyu Masuda.</p>
+  <footer class="mt-5 flex justify-center border-t mb-5">
+    <p class="my-5 font-thin text-xs">© Tenzyu Masuda</p>
   </footer>
 )
