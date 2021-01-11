eb872094bd73842c8628a8a89b4c28d2e129c0ec
tenzyu <tenzyu.on@gmail.com>
2021-01-11 22:04:34 +0900

    added year of copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default () => (
   <footer class="mt-5 flex justify-center border-t mb-5">
-    <p class="my-5 font-thin text-xs">© Tenzyu Masuda</p>
+    <p class="my-5 font-thin text-xs">© 2021 Tenzyu Masuda</p>
   </footer>
 )
