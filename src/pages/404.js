e1cb77faef270b8eb3465d3646ea90efb17b8bbb
tenzyu <tenzyu.on@gmail.com>
2021-01-11 22:26:50 +0900

    styled 404page

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,9 +7,9 @@ import { Link } from "gatsby"
 export default () => (
     <Layout>
         <Meta title="404" />
-        <main class="text-center">
-          <h2 class="my-16 text-4xl">Page Not Found</h2>
-          <h3 class="text-blue-500 text-xl"><Link to="/">Go Back</Link></h3>
+        <main class="text-center space-y-8 my-8">
+          <h2 class="text-3xl">ページが見つかりませんでした。</h2>
+          <h3 class="text-2xl text-blue-500"><Link to="/">ホームに戻る</Link></h3>
         </main>
     </Layout>
 );
