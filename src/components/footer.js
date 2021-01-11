b9df3db510e1541cdf2f8e34c95a57030d56178b
tenzyu <tenzyu.on@gmail.com>
2021-01-11 22:08:15 +0900

    styled footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <footer class="mt-5 flex justify-center border-t mb-5">
-    <p class="my-5 font-thin text-xs">© 2021 Tenzyu Masuda</p>
+  <footer class="mt-4 py-8 flex justify-center border-t">
+    <p class="font-thin text-xs">© 2021 Tenzyu Masuda</p>
   </footer>
 )
