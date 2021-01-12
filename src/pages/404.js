74bf39ad29ce88dca3562e41b95e6f98d58b5dda
tenzyu <tenzyu.on@gmail.com>
2021-01-12 18:42:40 +0900

    minor fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,16 @@ import Layout from "../layouts/default"
 import Meta from "../components/meta"
 import { Link } from "gatsby"
 
-
-export default () => (
+const NotFoundPage = () => {
+  return(
     <Layout>
-        <Meta title="404" />
-        <main class="text-center space-y-8 my-8">
+      <Meta title="404" />
+      <main class="text-center space-y-8 my-8">
         <h2 class="text-xl md:text-3xl">ページが見つかりませんでした。</h2>
         <h3 class="text-xl md:text-2xl text-blue-500"><Link to="/">ホームに戻る</Link></h3>
-        </main>
-    </Layout>
-);
+      </main>
+  </Layout>
+  )
+}
+
+export default NotFoundPage
