6cd0e4cac0719e9f6e3864f8a16ff2886a23fdc0
tenzyu <tenzyu.on@gmail.com>
2021-01-12 18:42:10 +0900

    better styled

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ export default () => (
     <Layout>
         <Meta title="404" />
         <main class="text-center space-y-8 my-8">
-          <h2 class="text-3xl">ページが見つかりませんでした。</h2>
-          <h3 class="text-2xl text-blue-500"><Link to="/">ホームに戻る</Link></h3>
+        <h2 class="text-xl md:text-3xl">ページが見つかりませんでした。</h2>
+        <h3 class="text-xl md:text-2xl text-blue-500"><Link to="/">ホームに戻る</Link></h3>
         </main>
     </Layout>
 );
