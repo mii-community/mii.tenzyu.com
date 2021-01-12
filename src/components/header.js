d7c8bebe05e08a02a96d72a6034686f6648dabdb
tenzyu <tenzyu.on@gmail.com>
2021-01-12 19:38:03 +0900

    linked credits page from header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const navs = [
+  {
+    name: "Credits",
+    link: "/credits/",
+  }
+]
 export default () => (
-  <header class="font-logo text-xl h-16 mb-4 flex justify-around sticky top-0 left-0 z-50 opacity-90 bg-white border-b">
-    <h1 class="my-auto">
+  <header class="font-logo h-16 mb-4 flex justify-around sticky top-0 left-0 z-50 opacity-90 bg-white border-b">
+    <h1 class="my-auto text-xl">
       <Link to={"/"}>HOME</Link>
     </h1>
-    <nav class="my-auto w-1/2">
-      {/* <ul class="flex justify-around">
+    <nav class="my-auto w-1/3 text-xs">
+      <ul class="flex justify-around">
       {navs.map(nav => (
         <li><Link to={nav.link}>{nav.name}</Link></li>
           ))}
-      </ul> */}
+      </ul>
     </nav>
   </header>
 )
