4c041dcfb111a04431ca82ef6c6210fd828ef037
tenzyu <tenzyu.on@gmail.com>
2021-01-17 05:59:43 +0900

    fixed font-size on header

--- src/components/header.js
+++ src/components/header.js
@@ -14,10 +14,10 @@ const navs = [
 
 export default () => (
   <header class="h-16 mb-4 flex justify-around sticky top-0 left-0 z-50 opacity-90 bg-white border-b">
-    <h1 class="my-auto text-xl">
+    <h1 class="my-auto text-lg">
       <Link to={"/"}>HOME</Link>
     </h1>
-    <nav class="my-auto w-1/2">
+    <nav class="my-auto w-1/3 text-xs lg:text-base">
       <ul class="flex justify-around">
         {navs.map((nav) => (
           <li>
