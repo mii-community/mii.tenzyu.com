54b3d90092a7737341f002c63890c0b66859d028
tenzyu <tenzyu.on@gmail.com>
2021-01-17 01:07:07 +0900

    installed plugins to render images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: "mii.tenzyu.com",
   },
-  plugins: ["gatsby-plugin-react-helmet", `gatsby-plugin-postcss`],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-postcss`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
